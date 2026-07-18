0ceaceaa583f685f4b5eed523be519950ecefc30
Stanislav Khrapov <khrapovs@gmail.com>
2026-07-18 14:21:39 +0200

    refactor: remove simulation package exports from __init__.py

--- src/order_matching/simulation/__init__.py
+++
@@ -1,7 +0,0 @@
-from order_matching.simulation.base_trader import BaseTrader
-from order_matching.simulation.market import Market
-from order_matching.simulation.market_view import MarketView
-from order_matching.simulation.news_feed import NewsEvent, NewsFeed
-from order_matching.simulation.noise_trader import NoiseTrader
-
-__all__ = ["NewsEvent", "NewsFeed", "MarketView", "BaseTrader", "NoiseTrader", "Market"]
